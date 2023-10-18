e512a8ff9d5a96a2b98017a69f8b05971ef4559e
Eva2626 <evoti1985@gmail.com>
2023-10-18 18:01:14 +0200

    subida contacts

---
+++ POO/contacts.js
@@ -0,0 +1,16 @@
+class contact {
+
+    constructor(){
+        this.contacts = [];
+    
+    }
+
+//metodos
+printPersons (){
+    console.log(this.contacts); 
+}
+
+}
+
+module.exports = {contact}; 
+
